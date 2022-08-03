2c47002de1488732fcf85d15b947f188d862e8e9
FranKBrownn <Frankcdbrown97@gmail.com>
2022-08-03 10:32:02 +0000

    added basic structure to the run.py flie and created a basic introduction to the game

--- run.py
+++ run.py
@@ -1,3 +1,70 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randint
+
+scores = {"computer": 0, "player": 0}
+
+class board:
+
+    """
+    the board where the game is played by adding the number of ships to random 
+    postions, players name to both the player and the computer
+    """
+
+    def __init__(self, size, num_ships, name, type):
+        self.size = size
+        self.board = [["." for x in range(size)] for y in range(size)]
+        self.num_ships = num_ships
+        self.name = name
+        self.type = type
+        self.guesses = []
+        self.ships = []
+
+    def print(self):
+        for row in self.board:
+            print(" ".join(row))   
+
+    def guess(self, x, y):
+        self.guesses.append(x, y)    
+        self.board[x] [y] = "x"
+
+        if (x, y) in self.ships:
+            self.board[x] [y] = "*"
+            return "Hit"
+        else:
+            return "Miss"
+
+    def add_ship(self, x, y, type="AI"):
+        if len(self.ships) >= self.num_ships:
+            print("Error max number of ships reached")     
+        else:
+            self.ships.append((x, y))      
+            if self.type == "player":
+                self.board [x] [y] ="@"    
+
+def random_point(size):
+    """
+    creats a random number between 0 and the size of the board
+    """             
+    return randint(0, size - 1)
+
+def new_game():
+    """
+    starts a new game restarting all scores and reseting ship postions
+    """
+    size = 5
+    num_ships = 4
+    scores["AI"] = 0
+    scores["player"] = 0
+    print("-" * 35)
+    print("welcome to battleships supreme")
+    print(f"board size: {size}. number of ships: {num_ships}")
+    print("top left corner is row: 0  col: 0")
+    print("-" * 35)
+    player_name = input("please enter your name: \n")
+    print("-" * 35)
+
+new_game()        
+
+
+
+
+
