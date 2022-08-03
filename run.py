c1a0747e1826474d8faa27eaba3f46312d1c6666
FranKBrownn <Frankcdbrown97@gmail.com>
2022-08-03 15:33:20 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,79 +1,42 @@
-from random import randint
 
-scores = {"computer": 0, "player": 0}
-
-
-class Board:
+def create_boards():
+    """ 
+    generates the boards for both player and comupter
     """
-    the board where the game is played by adding the number of ships to random 
-    postions, players name to both the player and the computer
-    """
+    print("your board")
+    row = 5
+    col = 5
+    for i in range(row):
+        for j in range(col):
+            print(" . ", end="")
+        print()
 
-    def __init__(self, size, num_ships, name, type):
-        self.size = size
-        self.board = [["." for x in range(size)] for y in range(size)]
-        self.num_ships = num_ships
-        self.name = name
-        self.type = type
-        self.guesses = []
-        self.ships = []
+    print("computer's board")
+    for i in range(row):
+        for j in range(col):
+            print(" . ", end="")
+        print()        
 
-    def print(self):
-        for row in self.board:
-            print(" ".join(row))   
-
-    def guess(self, x, y):
-        self.guesses.append(x, y)    
-        self.board[x][y] = "x"
-
-        if (x, y) in self.ships:
-            self.board[x][y] = "*"
-            return "Hit"
-        else:
-            return "Miss"
-
-    def add_ship(self, x, y, type="AI"):
-        if len(self.ships) >= self.num_ships:
-            print("Error max number of ships reached")     
-        else:
-            self.ships.append((x, y))      
-            if self.type == "player":
-                self.board[x][y] = "@"    
-
-
-def random_point(size):
-    """
-    creats a random number between 0 and the size of the board
-    """             
-    return randint(0, size - 1)
 
 
 def new_game():
-    """
-    starts a new game restarting all scores and reseting ship postions
-    """
-    size = 5
-    num_ships = 4
-    scores["AI"] = 0
-    scores["player"] = 0
-    print("-" * 35)
-    print("welcome to battleships supreme")
-    print(f"board size: {size}. number of ships: {num_ships}")
-    print("top left corner is row: 0  col: 0")
-    print("-" * 35)
-    player_name = input("please enter your name: \n")
-    print("-" * 35)
 
-    ai_board = Board(size, num_ships, "AI", type="AI")
-    player_board = Board(size, num_ships, player_name, type="player")
+    """
+    gives and introduction to the player and creats the board with randome 
+    ships ready to play the game
+        """
+    print("welcome to battle ships supreame")
+    player_name = input("please enter your name\n")
+    print(f"welcome {player_name} get ready for some battleship warfare!!!\n")
+    print(f"{player_name}'s battle felid")
 
-    populate_board(ai_board)
-    populate_board(player_board)
+    create_boards()
+
+
+new_game()
+
+
+
     
-
-new_game()        
-
-
-
-
+    
 
